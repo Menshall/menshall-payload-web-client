42bb37afe6543bb06cbd21d47bb06022d0198f16
andriymaslovskyy <maslovskiy2517@gmail.com>
2024-11-07 16:24:29 +0200

    add clarity heatmap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,6 +77,20 @@ export default async function RootLayout({
 
         <link rel="icon" href="/favicon.ico" sizes="32x32" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <Script
+          id="clarity"
+          type="text/javascript"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+                  (function(c,l,a,r,i,t,y){
+                      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                  })(window, document, "clarity", "script", "ouvfmjtk7t");
+          `,
+          }}
+        ></Script>
         {/* eslint-disable-next-line @next/next/inline-script-id */}
         <Script
           strategy="afterInteractive"
